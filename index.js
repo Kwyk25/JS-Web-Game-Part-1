4e5f7690c7fe183bf96ec95880e989306e6d2154
kyle <kylev006@gmail.com>
2023-02-27 23:05:35 -0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+function newImage  (src, left, bottom){
+ let item = document.createElement("img")
+    item.src = src
+    item.position = "fixed"
+    item.style.left = left + "px"
+    item.style.bottom = bottom + "px"
+    document.body.append(item)
+    return item
+
+}
+function newItem(url, left, bottom){
+    let item = newImage(url, left, bottom)
+
+    item.addEventListener("dblclick",() =>{
+        item.remove()
+    })
+}
+
+newImage("assets/green-Character.gif", 100, 100)
+newImage("assets/pine-tree.png", 450, 200)
+newImage("assets/tree.png", 200, 300)
+newImage("assets/pillar.png", 350, 100)
+newImage("assets/crate.png", 150, 200)
+newImage("assets/well.png", 500, 425)
+newItem("assets/sword.png", 500, 405)
+newItem("assets/sheild.png", 165, 185)
+newItem("assets/staff.png", 600, 100)
